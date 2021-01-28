b7338e1d67a78cf3264ad0b3cb187f1926efb44f
Michael Militar <mikeymilitar10@gmail.com>
2021-01-27 20:01:30 -0500

    finished search/fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Movie from './components/Movie'
-import './App.css'
+import './index.css'
 
 const url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a65f9b291a589567b929f15ba75cf51a&query=";
 const search = "https://api.themoviedb.org/3/search/movie?api_key=a65f9b291a589567b929f15ba75cf51a&query="
@@ -11,27 +11,27 @@ function App() {
   const [searchTerm, setSearch] = useState('')
 
   useEffect(() => {
-    fetch(url)
+    getMovies(url)
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results)
       })
+  }
 
-  }, [])
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
 
-    if (searchTerm){
-
-    fetch(search + searchTerm)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results)
-      })
+    if (searchTerm) {
+      getMovies(search + searchTerm)
       setSearch('')
     }
   }
+
   const handleOnChange = (e) => {
     setSearch(e.target.value)
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,16 +2,29 @@ import React from 'react'
 
 const imag = "https://image.tmdb.org/t/p/w1280"
 
+const setVoteClass = (vote) => {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 6) {
+        return 'green'
+    } else {
+        return 'red'
+    }
+}
+
+
 const Movie =
-    ({ title, poster_path, overview, vote_average}) => (
+    ({ title, poster_path, overview, vote_average }) => (
         <div className="movie">
             <img src={imag + poster_path} alt={title} />
-            <div className = "movie-info">
+            <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <span className={`tag ${setVoteClass(vote_average)}`}>
+                    {vote_average}
+                </span>
             </div>
 
-            <div className = "movie-over">
+            <div className="movie-over">
                 <h2>Overview: </h2>
                 <p>{overview}</p>
             </div>
